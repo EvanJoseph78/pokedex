c512772e9e863aad8015c4a6a9c98932ac759a1e
Evan Joseph <evandromariano49@gmail.com>
2023-08-21 18:24:50 -0300

    feat: pokemon-model

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,26 @@
 const pokeApi = {};
 
+function convertPokeApiDetailToPokemon(pokemonDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokemonDetail.order;
+  pokemon.name = pokemonDetail.name;
+
+  const types = pokemonDetails.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+  pokemon.types = types;
+  pokemon.type = type;
+
+  pokemon.photo = pokemon.sprites.other.dream_world.front_default;
+
+  pokemon.types = pokemon.type = pokemon.types.get(0);
+
+  return pokemon;
+}
+
 pokeApi.getPokemonsDeatils = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json());
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon);
 };
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
